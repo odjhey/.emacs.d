c00cb70640553e3b5308432bfadc966d14b1f7be
odz <odjhey@gmail.com>
2021-03-29 03:02:19 +0800

    remap keys for the gutter hydra

--- init.el
+++ init.el
@@ -482,21 +482,21 @@ _p_rint
                             :hint nil)
   "
 Git gutter:
-  _j_: next hunk        _s_tage hunk     _q_uit
-  _k_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
-  ^ ^                   _p_opup hunk
+  _n_: next hunk        _s_tage hunk     _q_uit
+  _p_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
+  ^ ^                   _P_opup hunk
   _h_: first hunk
   _l_: last hunk        set start _R_evision
 "
-  ("j" git-gutter:next-hunk)
-  ("k" git-gutter:previous-hunk)
+  ("n" git-gutter:next-hunk)
+  ("p" git-gutter:previous-hunk)
   ("h" (progn (goto-char (point-min))
               (git-gutter:next-hunk 1)))
   ("l" (progn (goto-char (point-min))
               (git-gutter:previous-hunk 1)))
   ("s" git-gutter:stage-hunk)
   ("r" git-gutter:revert-hunk)
-  ("p" git-gutter:popup-hunk)
+  ("P" git-gutter:popup-hunk)
   ("R" git-gutter:set-start-revision)
   ("q" nil :color blue)
   ("Q" (progn (git-gutter-mode -1)
