84b00e1c6906d69e5a1d3a51ce345ad6bcd7d2c0
odz <odjhey@gmail.com>
2021-03-29 02:46:27 +0800

    add hydra-git-gutter FTW

--- init.el
+++ init.el
@@ -477,6 +477,36 @@ _p_rint
   ("Buffer"
     (("b" switch-to-buffer "switch-to-buffer"))))
 
+
+(defhydra hydra-git-gutter (:body-pre (git-gutter-mode 1)
+                            :hint nil)
+  "
+Git gutter:
+  _j_: next hunk        _s_tage hunk     _q_uit
+  _k_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
+  ^ ^                   _p_opup hunk
+  _h_: first hunk
+  _l_: last hunk        set start _R_evision
+"
+  ("j" git-gutter:next-hunk)
+  ("k" git-gutter:previous-hunk)
+  ("h" (progn (goto-char (point-min))
+              (git-gutter:next-hunk 1)))
+  ("l" (progn (goto-char (point-min))
+              (git-gutter:previous-hunk 1)))
+  ("s" git-gutter:stage-hunk)
+  ("r" git-gutter:revert-hunk)
+  ("p" git-gutter:popup-hunk)
+  ("R" git-gutter:set-start-revision)
+  ("q" nil :color blue)
+  ("Q" (progn (git-gutter-mode -1)
+              ;; git-gutter-fringe doesn't seem to
+              ;; clear the markup right away
+              (sit-for 0.1)
+              (git-gutter:clear))
+       :color blue))
+
+
 ;; add hydra for org-refile/ing
 
 (general-define-key :states '(normal visual insert emacs)
@@ -490,7 +520,9 @@ _p_rint
  "f" '(avy-goto-char-timer :which-key "goto char")
  "g" '(magit-status :which-key "magit")
  "c" '(hydra-control-tower/body :which-key "control tower")
- "b" '(hydra-buffer/body :which-key "buffers"))
+ "b" '(hydra-buffer/body :which-key "buffers")
+ "y" '(hydra-git-gutter/body :which-key "git gutter"))
+ 
 
 (winner-mode +1)
 
